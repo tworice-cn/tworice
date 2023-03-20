67d7f36b2a052e20e766a1b5cba5078635810326
二饭 <1786705138@qq.com>
2023-03-20 22:40:17 +0800

    【依赖更新】依赖更新

--- tworice-admin/src/test/java/Test.java
+++ tworice-admin/src/test/java/Test.java
@@ -4,14 +4,40 @@ import com.alibaba.fastjson.JSON;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Test {
 
+    public final static String lock = "1";
+
+
     public static void main(String[] args) {
-        Integer a = new Integer(1);
-        Integer[] arr = new Integer[]{a};
-        Object[] arrr = new Object[]{arr};
-        System.out.println(JSON.toJSONString(arrr));
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 5; i++) {
+            executorService.submit(() -> {
+                test();
+            });
+        }
+    }
+
+    public static void test() {
+        System.out.println(1);
+        synchronized (lock) {
+            test1();
+        }
+
+        System.out.println(3);
+    }
+
+    public static void test1() {
+        System.out.println(2);
+        System.out.println(2);
+        System.out.println(2);
+        System.out.println(2);
+        System.out.println(2);
+        System.out.println(2);
+        System.out.println(2);
     }
 
 }
