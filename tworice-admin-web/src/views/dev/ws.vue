3a4165bb5e2cf444ad8e9f4b800abe2a749754cb
二饭 <1786705138@qq.com>
2024-04-23 13:56:16 +0800

    【更新】WS开发界面样式添加

--- tworice-admin-web/src/views/dev/ws.vue
+++ tworice-admin-web/src/views/dev/ws.vue
@@ -1,77 +1,129 @@
 <template>
-  <div>
-    <input type="text" placeholder="输入所在组标识" v-model="group"/>
-    <button @click="connect">连接服务器</button>
-    <button @click="sendMessage">发送消息</button>
-    <!-- <button @click="close">关闭连接</button> -->
-    <br/>
-    <input type="text" placeholder="输入接收方标识" v-model="to"/>
-    <input type="text" placeholder="输入消息类型" v-model="type"/>
-    <input type="text" placeholder="输入发送内容" v-model="message"/>
-
-    <textarea placeholder="这里展示收到的消息" v-model="textarea" disabled></textarea>
-
-    
-  </div>
+    <div class="message-sender">
+        <el-form ref="form" :model="form" label-width="120px">
+            <el-form-item label="所在分组">
+                <el-select v-model="form.group" placeholder="请选择分组">
+                    <el-option
+                        v-for="item in groups"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value">
+                    </el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="发送给谁">
+                <el-input v-model="form.receiver" placeholder="请输入接收者标识"></el-input>
+            </el-form-item>
+            <el-form-item label="发送模式">
+                <el-select v-model="form.mode" placeholder="请选择发送模式">
+                    <el-option
+                        v-for="item in modes"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value">
+                    </el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="发送内容">
+                <el-input type="textarea" v-model="form.message" ></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="warning" @click="connect">连接服务器</el-button>
+                <el-button type="primary" @click="sendMessage">发送消息</el-button>
+            </el-form-item>
+        </el-form>
+        <el-divider></el-divider>
+        
+        <el-input
+            type="textarea"
+            v-model="receivedMessages"
+            disabled>
+        </el-input>
+        
+    </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      socket: null,
-      group: '',
-      textarea: '',
-      to:'1',
-      type:'TO_ONE',
-      message:''
-    };
-  },
-  methods: {
-    connect() {
-      const socket = new WebSocket('ws://127.0.0.1:18080/websocket?t=1');
-      socket.addEventListener('open', (event) => {
-        console.log('Connected to WebSocket');
-        this.socket = socket;
-
-        let data = {
-          uid:JSON.parse(window.localStorage.getItem('admin')).id,
-          type:'LOGIN'
-        }
-        if(this.group){
-          data.gid=this.group
-        }
-        socket.send(JSON.stringify(data));
-      });
-
-      socket.addEventListener('message', (event) => {
-        console.log('收到消息:', event.data);
-        this.textarea=this.textarea+'\n'+event.data;
-      });
-
-      socket.addEventListener('close', (event) => {
-        console.log('连接断开');
-      });
-
-      socket.addEventListener('error', (event) => {
-        console.error('WebSocket error:', event);
-      });
-
-      socket.addEventListener('beforeopen', (event) => {
-        socket.setRequestHeader('Authorization', '123456');
-      });
+    data() {
+        return {
+            form: {
+                group: 1,
+                receiver: '1', // 发送给谁
+                message: '',
+                mode: 'TO_GROUP'
+            },
+            groups: [{label: '山西太原', value: 1}],
+            modes: [{label: '单聊', value: 'TO_ONE'}, {label: '群聊', value: 'TO_GROUP'}, {label: '广播', value: 'TO_ALL'}],
+            receivedMessages: '', // 收到的消息
+            socket: null,
+        };
     },
-    sendMessage() {
-      if (this.socket && this.socket.readyState === WebSocket.OPEN) {
-        let message = {
-          uid:JSON.parse(window.localStorage.getItem('admin')).id,
-          content:this.message,
-          to:this.to,
-          type:this.type
-        }
-        this.socket.send(JSON.stringify(message));
-      }
+    methods: {
+        connect() {
+            const socket = new WebSocket('ws://127.0.0.1:18080/websocket');
+            socket.addEventListener('open', (event) => {
+                this.$message({
+                    type: 'success',
+                    message: '连接成功'
+                })
+                this.socket = socket;
+                
+                let data = {
+                    uid: JSON.parse(window.localStorage.getItem('admin')).id,
+                    type: 'LOGIN'
+                }
+                if (this.form.group) {
+                    data.gid = this.form.group
+                }
+                socket.send(JSON.stringify(data));
+            });
+            
+            socket.addEventListener('message', (event) => {
+                console.log('收到消息:', event);
+                this.messageList.push(JSON.parse(event.data))
+                // this.receivedMessages = this.receivedMessages + '\n' + JSON.parse(event.data).message;
+            });
+            
+            socket.addEventListener('close', (event) => {
+                console.log('连接断开:',event)
+                this.$message({
+                    type: 'warning',
+                    message: '连接断开'
+                })
+            });
+            
+            socket.addEventListener('error', (event) => {
+                console.error('WebSocket error:', event);
+            });
+            
+            socket.addEventListener('beforeopen', (event) => {
+                socket.setRequestHeader('Authorization', '123456');
+            });
+        },
+        sendMessage() {
+            if (this.socket && this.socket.readyState === WebSocket.OPEN) {
+                let message = {
+                    uid: JSON.parse(window.localStorage.getItem('admin')).id,
+                    content: this.form.message,
+                    to: this.form.receiver,
+                    type: this.form.mode
+                }
+                console.log('发送消息:', message)
+                this.socket.send(JSON.stringify(message));
+            } else {
+                this.$message({
+                    type: 'error',
+                    message: '连接失败'
+                })
+            }
+        },
     },
-  },
 };
 </script>
+<style lang="less" scoped>
+.message-sender {
+    max-width: 600px;
+    margin: 30px auto;
+}
+</style>
