0e10efc17a212021944cd369f023cdbf657c2d47
二饭 <1786705138@qq.com>
2024-07-30 21:18:32 +0800

    perf:Optimize routing, add default routing and 404 routing

--- tworice-admin-web/src/core/router.js
+++ tworice-admin-web/src/core/router.js
@@ -3,45 +3,72 @@ import Router from "vue-router";
 
 Vue.use(Router);
 
-export const constantRoutes = [{
-    path: "/login",
-    component: () => import("@/views/login/index5.vue"),
-    meta: {title: "登录"}
-}, {
-    path: "/bigScreen",
-    component: () => import("@/views/admin/home.vue"),
-    meta: {title: "实时监控"}
-}, {
-    path: "/download/:id",
-    component: () => import("@/views/admin/file/download.vue"),
-    meta: {title: "下载中心"}
-}, {
-    path: "/admin/userCenter/:id",
-    component: () => import("@/views/admin/userCenter/userCenter.vue"),
-    meta: {title: "用户中心"}
-}, {
-    path: "/home", /*客户端*/
-    name: "欢迎",
-    component: () => import("@/views/front/index/index.vue"),
-    children: [],
-}, {
-    path: "/admin",
-    component: () => import("@/views/admin/index"),
-    children: [
-        {
-            path: "/admin/home",
-            name: "首页",
-            component: () => import("@/views/admin/home.vue"),
-            meta: {title: "首页"}
-        },
-        {
-            path: "showNotice",
-            name:'查看公告',
-            component: () => import("@/views/admin/showNotice.vue"),
-            meta: {title: "查看公告"}
-        }
-    ],
-}];
+export const constantRoutes = [
+    {
+        path: "/",
+        redirect: "/admin"
+    },
+    {
+        path: "/login",
+        component: () => import("@/views/login/index5.vue"),
+        meta: {title: "登录"}
+    }, {
+        path: "/bigScreen",
+        component: () => import("@/views/admin/home.vue"),
+        meta: {title: "实时监控"}
+    }, {
+        path: "/download/:id",
+        component: () => import("@/views/admin/file/download.vue"),
+        meta: {title: "下载中心"}
+    }, {
+        path: "/admin/userCenter/:id",
+        component: () => import("@/views/admin/userCenter/userCenter.vue"),
+        meta: {title: "用户中心"}
+    }, {
+        path: "/home", /*客户端*/
+        name: "欢迎",
+        component: () => import("@/views/front/index/index.vue"),
+        children: [],
+    }, {
+        path: "/admin",
+        component: () => import("@/views/admin/index"),
+        children: [
+            {
+                path: "/admin/home",
+                name: "首页",
+                component: () => import("@/views/admin/home.vue"),
+                meta: {title: "首页"}
+            },
+            {
+                path: "showNotice",
+                name: '查看公告',
+                component: () => import("@/views/admin/showNotice.vue"),
+                meta: {title: "查看公告"}
+            }
+        ],
+    },
+    {
+        path: "/404",
+        component: () => import("@/views/admin/404/404.vue"),
+        meta: {title: "404"}
+    },
+    {
+        path: "*",
+        redirect: "/404"
+    }
+];
+
+// 防止连续点击多次路由报错
+let routerPush = Router.prototype.push;
+let routerReplace = Router.prototype.replace;
+// push
+Router.prototype.push = function push(location) {
+    return routerPush.call(this, location).catch(err => err)
+}
+// replace
+Router.prototype.replace = function push(location) {
+    return routerReplace.call(this, location).catch(err => err)
+}
 
 export default new Router({
     scrollBehavior: () => ({y: 0}),
