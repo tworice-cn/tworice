d54f6842ac1468afc391f54ac40d9984752d9300
二饭 <1786705138@qq.com>
2025-02-09 19:03:42 +0800

    fix: 修复用户端无法展示子菜单问题

--- tworice-admin-web/src/components/commons/portalMenu.vue
+++ tworice-admin-web/src/components/commons/portalMenu.vue
@@ -1,37 +1,37 @@
 <template>
-      <div>
-            <el-menu-item class="logo-box">
-                  <i>
-                        <img src="favicon.ico" alt="">
-                  </i>
-                  <span slot="title" class="system-title">{{$setting.systemName}}</span>
+    <div>
+        <el-menu-item class="logo-box">
+            <i>
+                <img src="favicon.ico" alt="">
+            </i>
+            <span slot="title" class="system-title">{{ $setting.systemName }}</span>
+        </el-menu-item>
+        <template v-for="item in menu">
+            <el-submenu v-if="item.type===1" :index="item.id + '' " :key="item.id">
+                <template slot="title">
+                    <i :class="item.icon"></i>
+                    <span>{{ item.name }}</span>
+                </template>
+                <el-menu-item v-for="menuItem in item.children" :key="menuItem.id" :index="menuItem.url">
+                    {{ menuItem.name }}
+                </el-menu-item>
+            </el-submenu>
+            <el-menu-item v-if="item.type===2 && item.menu" :key="item.id" :index="item.url">
+                {{ item.name }}
             </el-menu-item>
-            <template v-for="item in menu">
-                  <el-submenu v-if="item.type==1" :index="item.id + '' " :key="item.id">
-                        <template slot="title">
-                              <i :class="item.icon"></i>
-                              <span>{{item.name}}</span>
-                        </template>
-                        <el-menu-item v-for="menuItem in item.list" :key="menuItem.id" :index="menuItem.url">
-                              {{menuItem.name}}
-                        </el-menu-item>
-                  </el-submenu>
-                  <el-menu-item v-if="item.type==2 && item.menu" :key="item.id" :index="item.url">
-                        {{item.name}}
-                  </el-menu-item>
-            </template>
-      </div>
+        </template>
+    </div>
 </template>
 
 <script>
 
 export default {
-      name: "PortalMenu",
-      props: ["menu"],
-      data() {
-            return {};
-      },
-      methods: {},
+    name: "PortalMenu",
+    props: ["menu"],
+    data() {
+        return {};
+    },
+    methods: {},
 };
 </script>
 
