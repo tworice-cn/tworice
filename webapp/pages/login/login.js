116e6a08c42be30b285e53333ec2adebd4ee63f6
二饭 <1786705138@qq.com>
2025-03-31 15:30:01 +0800

    fix: 微信小程序适配服务端参数的调整

--- webapp/pages/login/login.js
+++ webapp/pages/login/login.js
@@ -1,97 +1,97 @@
 // pages/login/login.js
-const host=getApp().host;
+const host = getApp().host;
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    loginName:'',
-    passWord:'',
-    key:'',
-    captcha:'',
-    verCode:''
-  },
-  initCaptcha(){
-    getApp().request.get('/admin/login/captcha').then(res=>{
-      if(res.data.status.code==204){
-        wx.showModal({
-          title: '提示',
-          content: '获取验证码失败',
-          showCancel:false,
-          confirmText:'重新获取',
-          success:()=>{
-            this.initCaptcha()
-          }
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        loginName: '',
+        passWord: '',
+        key: '',
+        captcha: '',
+        verCode: ''
+    },
+    initCaptcha() {
+        getApp().request.get('/admin/login/captcha').then(res => {
+            if (res.data.status.code == 204) {
+                wx.showModal({
+                    title: '提示',
+                    content: '获取验证码失败',
+                    showCancel: false,
+                    confirmText: '重新获取',
+                    success: () => {
+                        this.initCaptcha()
+                    }
+                })
+            } else {
+                this.setData({
+                    key: res.data.data.key,
+                    captcha: res.data.data.captcha
+                })
+            }
         })
-      }else{
-        this.setData({
-          key:res.data.data.key,
-          captcha:res.data.data.captcha
-        })
-      }
-    })
-  },
+    },
 
-  /**登录 */
-  login(){
-    wx.showLoading({
-      title: '正在登录',
-    })
-    getApp().request.post('/admin/login',{
-      loginName:this.data.loginName,
-      password:this.data.passWord,
-      key:this.data.key,
-      verCode:this.data.verCode
-    }).then(res=>{
-      console.log(res);
-      if(res.data.status.code==200){
-        wx.setStorageSync('token',res.data.data.token);
-        wx.setStorageSync('admin',res.data.data.admin);
-        wx.setStorageSync('role',res.data.data.roles[0]);
-        wx.switchTab({
-          url: '/pages/user/user'
+    /**登录 */
+    login() {
+        wx.showLoading({
+            title: '正在登录',
         })
-        wx.showToast({
-          title: '登录成功',
-          icon:'success'
+        getApp().request.post('/admin/login', {
+            loginName: this.data.loginName,
+            password: this.data.passWord,
+            key: this.data.key,
+            captcha: this.data.verCode
+        }).then(res => {
+            console.log(res);
+            if (res.data.status.code == 200) {
+                wx.setStorageSync('token', res.data.data.token);
+                wx.setStorageSync('admin', res.data.data.admin);
+                wx.setStorageSync('role', res.data.data.roles[0]);
+                wx.switchTab({
+                    url: '/pages/user/user'
+                })
+                wx.showToast({
+                    title: '登录成功',
+                    icon: 'success'
+                })
+            } else {
+                console.log(res);
+                wx.showToast({
+                    title: '登录失败',
+                    icon: 'none'
+                })
+                this.initCaptcha();
+            }
         })
-      }else{
-        console.log(res);
-        wx.showToast({
-          title: '登录失败',
-          icon:'none'
+    },
+    toReg() {
+        wx.navigateTo({
+            url: '/pages/reg/reg',
         })
+    },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+        wx.clearStorageSync();
         this.initCaptcha();
-      }
-    })
-  },
-  toReg(){
-    wx.navigateTo({
-      url: '/pages/reg/reg',
-    })
-  },
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    wx.clearStorageSync();
-    this.initCaptcha();
-  },
-  inputName(e){
-    this.setData({
-      loginName:e.detail.value
-    })
-  },
-  inputPwd(e){
-    this.setData({
-      passWord:e.detail.value
-    })
-  },
-  inputCaptcha(e){
-    this.setData({
-      verCode:e.detail.value
-    })
-  }
+    },
+    inputName(e) {
+        this.setData({
+            loginName: e.detail.value
+        })
+    },
+    inputPwd(e) {
+        this.setData({
+            passWord: e.detail.value
+        })
+    },
+    inputCaptcha(e) {
+        this.setData({
+            verCode: e.detail.value
+        })
+    }
 
 })
