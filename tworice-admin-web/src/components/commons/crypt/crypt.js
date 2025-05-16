9331ca3d71c14d2985151b5e267b6028892804ff
二饭 <1786705138@qq.com>
2025-05-16 22:07:49 +0800

    fix: 解决加密解密后特殊字符丢失问题

--- tworice-admin-web/src/components/commons/crypt/crypt.js
+++ tworice-admin-web/src/components/commons/crypt/crypt.js
@@ -15,17 +15,17 @@ export default {
     decrypt: function (data) {
         switch (data.type) {
             case 'AES':
-                return AesUtil.Decrypt(data.crypt).replace(/[^\x20-\x7E\u4E00-\u9FFF]+/g, "");
+                return AesUtil.Decrypt(data.crypt).replace(/\p{C}/gu, "");
             case 'DES':
-                return DesUtil.desDecrypt(data.crypt,null).replace(/[^\x20-\x7E\u4E00-\u9FFF]+/g, "");
+                return DesUtil.desDecrypt(data.crypt,null).replace(/\p{C}/gu, "");
             case 'SM4':
-                return SMCryptoUtil.sm4Decrypt(data.crypt, null, null).replace(/[^\x20-\x7E\u4E00-\u9FFF]+/g, "");
+                return SMCryptoUtil.sm4Decrypt(data.crypt, null, null).replace(/\p{C}/gu, "");
             case 'SM2+SM4':
-                return SMCryptoUtil.sm4Decrypt(data.crypt, null, null).replace(/[^\x20-\x7E\u4E00-\u9FFF]+/g, "");
+                return SMCryptoUtil.sm4Decrypt(data.crypt, null, null).replace(/\p{C}/gu, "");
             case 'Base64':
                 return decodeURIComponent(escape(atob(data.crypt)));
             default:
-                return AesUtil.Decrypt(data.crypt).replace(/[^\x20-\x7E\u4E00-\u9FFF]+/g, "");
+                return AesUtil.Decrypt(data.crypt).replace(/\p{C}/gu, "");
         }
     }
 }
